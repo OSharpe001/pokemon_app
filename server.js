47ff38805984bf7e299156b10ebd6054333cce25
O. Sharpe <osharpe001@gmail.com>
2023-08-18 21:19:21 -0400

    Show view shows req.params.id

--- server.js
+++ server.js
@@ -31,4 +31,11 @@ app.get("/pokemon", (req, res) => {
     });
 });
 
+app.get("/pokemon/:id", (req, res) => {
+    res.send(req.params.id);
+    // res.render("Index", {
+    //     pokemon: pokemon
+    // });
+});
+
 // nodemon
